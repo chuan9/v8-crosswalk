bdf21c0debe4546fa022e1ae41f5089b06def7f9
chuan9 <chuan.liu@intel.com>
2015-09-16 18:31:33 +0800

    Change type name from all lower case to first Character upper case in runtime BUG=XWALK-4766

--- test/mjsunit/simd/deopt.js
+++ test/mjsunit/simd/deopt.js
@@ -28,8 +28,8 @@
 // Flags: --simd-object --allow-natives-syntax
 
 function testdeopt(a, b) {
-  var a4 = SIMD.float32x4(1.0, -2.0, 3.0, -4.0);
-  var b4 = SIMD.float32x4.abs(a4);
+  var a4 = SIMD.Float32x4(1.0, -2.0, 3.0, -4.0);
+  var b4 = SIMD.Float32x4.abs(a4);
 
   if (a > 0) {
     a = 0;
@@ -49,16 +49,16 @@ testdeopt(1, 1);
 testdeopt(0, 1);
 
 function testdeopt2() {
-  var a4 = SIMD.float32x4(1.0, -1.0, 1.0, -1.0);
-  var b4 = SIMD.float32x4.abs(a4);
+  var a4 = SIMD.Float32x4(1.0, -1.0, 1.0, -1.0);
+  var b4 = SIMD.Float32x4.abs(a4);
 
   assertEquals(1.0, b4.x);
   assertEquals(1.0, b4.y);
   assertEquals(1.0, b4.z);
   assertEquals(1.0, b4.w);
 
-  var new_a4 = new SIMD.float32x4(1.0, -1.0, 1.0, -1.0);
-  var new_b4 = SIMD.float32x4.abs(new_a4);
+  var new_a4 = new SIMD.Float32x4(1.0, -1.0, 1.0, -1.0);
+  var new_b4 = SIMD.Float32x4.abs(new_a4);
 
   assertEquals(1.0, new_b4.x);
   assertEquals(1.0, new_b4.y);
